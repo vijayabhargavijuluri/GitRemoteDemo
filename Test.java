974345dd4ed891aff2ac216decae50d3111b39ae
vijayabhargavijuluri <33111080+vijayabhargavijuluri@users.noreply.github.com>
2017-10-27 16:05:34 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,5 +3,5 @@ public class Test{
 	public static void main(String args[]){
 
 	System.out.println("Hello World");
-	System.out.println(""Test is working");
+	System.out.println("Test is working");
 }}
